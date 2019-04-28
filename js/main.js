82454b75ca6d7ab78f8a9536cab84851bedd5e0f
mattperkins <github@mattperkins.com>
2019-04-28 08:01:38 +0100

    render html

--- js/main.js
+++ js/main.js
@@ -2,10 +2,34 @@ const search = document.getElementById("search")
 const matchList = document.getElementById("match-list")
 
 // search and filter json file
-const searchStates = async searchText => {
-  const data = await (await fetch('../data.json')).json()
+const searchTracks = async searchText => {
+  const tracks = await (await fetch('../data.json')).json()
 
-  console.log(data)
+  // Get matches
+  let matches = tracks.filter(track => {
+    const regex = new RegExp(`^${searchText}`, "gi")
+    return track.title.match(regex) || track.album.match(regex)
+  })
+  if (searchText.length === 0) {
+    matches = []
+    matchList = ""
+  }
+
+  outputHtml(matches)
 }
 
-search.addEventListener("input", () => searchStates(search.values))
+// Render results to HTML
+const outputHtml = matches => {
+  if (matches.length > 0) {
+    const html = matches.map(match => `
+      <div class="card card-body mb-1">
+        <h5>${match.title} <span class="text-primary">${match.album}</span></h5>
+      </div>
+    `)
+      .join("");
+
+    matchList.innerHTML = html;
+  }
+};
+
+search.addEventListener("input", () => searchTracks(search.value))
